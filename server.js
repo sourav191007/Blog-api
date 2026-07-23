04ebb160c3a7ef650942751322d96694254783cc
sourav191007 <kumarsourav191007@gmail.com>
2026-07-24 00:32:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://blog-api-b2tz.onrender.com";
+const API_URL = "https://your-api.onrender.com";
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
